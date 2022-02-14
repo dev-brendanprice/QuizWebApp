7307a481a93a4b2453a93652ce51502db02e0bb2
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-02-14 01:54:08 +0000

    latest

--- index.js
+++ index.js
@@ -12,17 +12,36 @@ const port = 3221;
 log(`- http://86.2.10.33:${port}/`);
 
 io.on('connection', (socket) => {
-    log(`${socket.id} attached`)
-    var number = 0;
+    log(`${socket.id} attached`);
+
+    var users = {};
 
     socket.on('createRoom', (room) => {
-        log("hi")
-        socket.join(room);
+        // Create Room
+        log('Room Created: ' + room);
+        socket.join(room.id);
+    });
+
+    socket.on('joinRoom', (room) => {
+        // Assign Client to room
+        log(room);
     });
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/home.html'));
+});
+
+app.get('/teacher', (req, res) => {
+    res.sendFile(path.join(__dirname, '/teacher.html'));
+});
+
+app.get('/student', (req, res) => {
+    res.sendFile(path.join(__dirname, '/student.html'));
+});
+
+app.get('*', (req, res) => {
+    res.send('Error 404: How did we get here?');
 });
 
 httpServer.listen(port);
