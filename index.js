0e1f81ceb976c3d6f680a9d3cfef3fd2d5ab21f4
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-02-16 03:27:18 +0000

    dev

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
-const express = require("express");
-const res = require("express/lib/response");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+const express = require('express'),
+    res = require('express/lib/response'),
+    { createServer } = require('http'),
+    { Server } = require('socket.io'),
+    path = require('path');
 const log = console.log.bind(console);
 
-const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {});
-const port = 3221;
+const app = express(),
+    httpServer = createServer(app),
+    port = 3221,
+    io = new Server(httpServer, {});
 log(`- http://86.2.10.33:${port}/`);
 
+// Objects to store users & rooms
+var __userStruct__ = {},
+    __roomStruct__ = {};
+
+
 io.on('connection', (socket) => {
-    log(`${socket.id} attached`);
 
-    var users = {};
+    socket.on('roomListener', (args) => {
 
-    socket.on('createRoom', (room) => {
-        // Create Room
-        log('Room Created: ' + room);
-        socket.join(room.id);
+        // Check for userType & config rooms
+        if (args.user.userType == 'student' && __roomStruct__[args.room.roomCode]) {
+            __userStruct__[socket.id] = 
+            { 
+                user: { userType: args.user.userType, isTeacher: false },
+                room: { roomCode: args.room.roomCode }
+            };
+
+            socket.join(args.room.id);
+            log('Student Joined Room!');
+        }
+        else if (args.user.userType == 'teacher' && !__roomStruct__[args.room.id]) {
+            __roomStruct__[args.room.id] =
+            {
+                inception: { time: new Date() },
+                room:      { roomCode: args.room.roomCode, roomName: args.room.rommName }
+            };
+            __userStruct__[socket.id] = 
+            {
+                user: { userType: args.user.userType, isTeacher: true },
+                room: { roomCode: args.room.roomCode }
+            };
+
+            socket.join(args.room.id);
+            log('Teacher Created Room!');
+        };
     });
 
-    socket.on('joinRoom', (room) => {
-        // Assign Client to room
-        log(room);
+    socket.on('disconnect', () => {
+        // This works somehow lmfao, If client disconnects remove all references (RAM efficient)
+        if (__userStruct__[socket.id]) {
+            let roomCode = __userStruct__[socket.id].room.roomCode;
+            delete __roomStruct__[roomCode];
+            delete __userStruct__[socket.id];
+        };
     });
 });
 
+
+
 app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/home.html'));
+});
+app.get('', (req, res) => {
     res.sendFile(path.join(__dirname, '/home.html'));
 });
 
